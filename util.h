d2815a65b5bebcca66946fe9cc8539818698ee85
lflei <leifengpku@163.com>
2020-10-04 17:29:33 +0800

    add listnode

--- util.h
+++ util.h
@@ -18,3 +18,40 @@ void printVector(vector<T> v) {
     cout << "]" << endl;
 }
 
+// ============  链表相关 =============
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void printList(ListNode* head) {
+    if (head == nullptr) {
+        std::cout << "list is null";
+    }   
+    while(head != nullptr) {
+        std::cout << head->val << " -> ";
+        head = head->next;
+    }   
+    std::cout << std::endl;
+}
+
+ListNode* makeLinkList(vector<int> vals) {
+    ListNode *head = nullptr;
+    ListNode *tail = nullptr;
+    for(auto v : vals) {
+        if (head == nullptr) {
+            head = new ListNode(v);
+            tail = head;
+        } else {
+            tail->next = new ListNode(v);
+            tail = tail->next;
+        }   
+    }   
+    return head;
+}
+
+// ======================================
